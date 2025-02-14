18d830041c733c73cff15864c9023f92234964d3
r-mehrotra <rohanmehrotra123@gmail.com>
2025-02-14 23:42:11 +0530

    Completed Problem 42.

---
+++ src/main/java/problems/Problem42TrapRainWater.java
@@ -0,0 +1,42 @@
+package problems;
+
+public class Problem42TrapRainWater {
+    /**
+     * Time Complexity O(n)
+     * Space Complexity O(n)
+     */
+    private void populateMaxLeftArray(int[] maxFromLeft, int[] height) {
+        maxFromLeft[0] = 0;
+        for (int i = 1; i < height.length; i++) {
+            maxFromLeft[i] = Math.max(maxFromLeft[i - 1], height[i - 1]);
+        }
+    }
+
+    private void populateMaxRightArray(int[] maxFromRight, int[] height) {
+        maxFromRight[maxFromRight.length - 1] = 0;
+        for (int i = height.length - 2; i >= 0; i--) {
+            maxFromRight[i] = Math.max(maxFromRight[i + 1], height[i + 1]);
+        }
+    }
+
+    private int calculateTrappedWater(int[] maxFromLeft, int[] maxFromRight, int[] height) {
+        int trappedWater = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            if (maxFromLeft[i] != 0 && maxFromRight[i] != 0) {
+                int trapped = Math.min(maxFromLeft[i], maxFromRight[i]) - height[i];
+                if (trapped > 0) {
+                    trappedWater += trapped;
+                }
+            }
+        }
+        return trappedWater;
+    }
+
+    public int trap(int[] height) {
+        int[] maxFromLeft = new int[height.length];
+        int[] maxFromRight = new int[height.length];
+        populateMaxLeftArray(maxFromLeft, height);
+        populateMaxRightArray(maxFromRight, height);
+        return calculateTrappedWater(maxFromLeft, maxFromRight, height);
+    }
+}
