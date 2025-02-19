935968afe302ca33463f8a476fd479feaffd23e5
r-mehrotra <rohanmehrotra123@gmail.com>
2025-02-19 22:20:15 +0530

    Added much more optimal solution.

--- src/main/java/problems/Problem567PermutationInString.java
+++ src/main/java/problems/Problem567PermutationInString.java
@@ -30,4 +30,44 @@ public class Problem567PermutationInString {
         }
         return false;
     }
+
+    private int getMatchCount(int[] count1, int[] count2) {
+        int matchCount = 0;
+        for (int i = 0; i < count1.length; i++) {
+            if (count1[i] == count2[i]) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    public boolean checkInclusionOptimal(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int[] charCount1 = setupCharCount(s1, 0, s1.length());
+        int[] charCount2 = setupCharCount(s2, 0, s1.length());
+        int matchCount = getMatchCount(charCount1, charCount2);
+        int i = 0, j = s1.length();
+        while (j < s2.length()) {
+            if (matchCount == 26) {
+                return true;
+            }
+            charCount2[s2.charAt(j) - 'a']++;
+            if (charCount1[s2.charAt(j) - 'a'] == charCount2[s2.charAt(j) - 'a']) {
+                matchCount++;
+            } else if ((charCount1[s2.charAt(j) - 'a'] + 1) == charCount2[s2.charAt(j) - 'a']) {
+                matchCount--;
+            }
+            charCount2[s2.charAt(i) - 'a']--;
+            if (charCount1[s2.charAt(i) - 'a'] == charCount2[s2.charAt(i) - 'a']) {
+                matchCount++;
+            } else if ((charCount1[s2.charAt(i) - 'a'] - 1) == charCount2[s2.charAt(i) - 'a']) {
+                matchCount--;
+            }
+            i++;
+            j++;
+        }
+        return matchCount == 26;
+    }
 }
